1e1a02c0d0eb500277e5f948e918715f95a813cf
Matthew Rappaport <catfan348@hotmail.com>
2024-05-03 04:39:34 -0400

    Set up version tagger to handle lightweight and annotated tags differently

--- versioning.sh
+++ versioning.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 BRANCH=$(git branch --show-current)
-TAG=$(git describe --tags --abbrev=0 HEAD)
+TAG=$(git describe --abbrev=0 HEAD)
 STEPS=$(git rev-list $TAG..HEAD --count)
 
-echo "$BRANCH: $TAG-$((STEPS + 1))" > version.tex
+LWTAG=$(git describe --tags --abbrev=0 HEAD)
+LWSTEPS=$(git rev-list $LWTAG..HEAD --count)
+
+
+if [ $TAG != $LWTAG ]
+then
+	echo "$BRANCH: $TAG-$((STEPS + 1)) $LWTAG-$((LWSTEPS + 1))" > version.tex
+else
+	echo "$BRANCH: $TAG-$((STEPS + 1))" > version.tex
+fi
